e2db99d0887b8a48bb1a386a60d4da806767d9db
vortix2950 <vortix2950@ksu.edu>
2024-09-11 14:36:54 +0300

    minor var changes

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
 
-
-
 let box = document.getElementById("box")
 
 
+
 setInterval(() => {
+    let rgb_range = 255
+    let percent_range = 100 
+    let width = Math.floor(Math.random() * percent_range);
+    let height = Math.floor(Math.random() * percent_range);
 
-    let width = Math.floor(Math.random() * 100);
-    let height = Math.floor(Math.random() * 100);
-
-    let r = Math.floor(Math.random() * 255);
-    let g = Math.floor(Math.random() * 255);
-    let b = Math.floor(Math.random() * 255);
+    let r = Math.floor(Math.random() * rgb_range);
+    let g = Math.floor(Math.random() * rgb_range);
+    let b = Math.floor(Math.random() * rgb_range);
     let a = Number(Math.random().toPrecision(2));
+
     let transformX = `translateX(${Math.floor(Math.random() * 90)}vw)`;
     let transformY = `translateY(${Math.floor(Math.random() * 90)}vh)`;
     let color = `rgba(${r},${g},${b},${a})`
@@ -23,15 +24,13 @@ setInterval(() => {
     box.style.transform = `${transformX} ${transformY}`
 
    
-
     let box_outline = `${Math.floor(Math.random() * 25)}px`
 
-    r = Math.floor(Math.random() * 255);
-    g = Math.floor(Math.random() * 255);
-    b = Math.floor(Math.random() * 255);
+    r = Math.floor(Math.random() * rgb_range);
+    g = Math.floor(Math.random() * rgb_range);
+    b = Math.floor(Math.random() * rgb_range);
     a = Number(Math.random().toPrecision(2));
     color = `rgba(${r},${g},${b},${a})`
     box.style.border = `${box_outline} solid ${color}`
 
 }, 2000);
-
