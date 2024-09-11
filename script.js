e10ea85951eecf24be0c98ba6d8c4d112bba175c
vortix2950 <vortix2950@ksu.edu>
2024-09-11 14:31:38 +0300

    added opacity

--- script.js
+++ script.js
@@ -16,21 +16,21 @@ setInterval(() => {
     let transformX = `translateX(${Math.floor(Math.random() * 90)}vw)`;
     let transformY = `translateY(${Math.floor(Math.random() * 90)}vh)`;
     let color = `rgba(${r},${g},${b},${a})`
-    console.log(a)
+
     box.style.backgroundColor = color;
     box.style.width = `${width}%`
     box.style.height = `${height}%`
     box.style.transform = `${transformX} ${transformY}`
 
+   
+
+    let box_outline = `${Math.floor(Math.random() * 25)}px`
+
     r = Math.floor(Math.random() * 255);
     g = Math.floor(Math.random() * 255);
     b = Math.floor(Math.random() * 255);
     a = Number(Math.random().toPrecision(2));
-
     color = `rgba(${r},${g},${b},${a})`
-
-    let box_outline = `${Math.floor(Math.random() * 25)}px`
-
     box.style.border = `${box_outline} solid ${color}`
 
 }, 2000);
