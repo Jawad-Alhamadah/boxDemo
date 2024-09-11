c7271f51a1771daaaa29c2692030e236188852ad
vortix2950 <vortix2950@ksu.edu>
2024-09-11 14:18:26 +0300

    box demo

--- script.js
+++ script.js
@@ -5,27 +5,22 @@ let box = document.getElementById("box")
 
 
 setInterval(() => {
- 
-
-
-    let width =Math.floor(Math.random() * 1000);
-    let height=Math.floor(Math.random() * 1000);
+    console.log(Math.floor(Math.random() * 255))
+    let width =Math.floor(Math.random() * 100);
+    let height=Math.floor(Math.random() * 100);
     
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
 
-    let transformX = `translateX(${Math.floor(Math.random() * 255)}px)`;
-    let transformY = `translateY(${Math.floor(Math.random() * 255)}px)`;
+    let transformX = `translateX(${Math.floor(Math.random() * 90)}vw)`;
+    let transformY = `translateY(${Math.floor(Math.random() * 90)}vh)`;
     let color= `rgb(${r},${g},${b})`
 
-
     box.style.backgroundColor = color ;
-    box.style.width= `${width}px`
-    box.style.height= `${height}px`
+    box.style.width= `${width}%`
+    box.style.height= `${height}%`
     box.style.transform= `${transformX} ${transformY}`
 
-    
-
-    
 }, 2000);
+
