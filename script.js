476dedb2d34d9ff941e4a4d09c01ffa7529d58a1
vortix2950 <vortix2950@ksu.edu>
2024-09-11 14:30:11 +0300

    added opacity

--- script.js
+++ script.js
@@ -5,33 +5,33 @@ let box = document.getElementById("box")
 
 
 setInterval(() => {
-   
-    let width =Math.floor(Math.random() * 100);
-    let height=Math.floor(Math.random() * 100);
-    
+
+    let width = Math.floor(Math.random() * 100);
+    let height = Math.floor(Math.random() * 100);
+
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
-    let a = Number(Math.random().toPrecision(2)) ;
+    let a = Number(Math.random().toPrecision(2));
     let transformX = `translateX(${Math.floor(Math.random() * 90)}vw)`;
     let transformY = `translateY(${Math.floor(Math.random() * 90)}vh)`;
-    let color= `rgba(${r},${g},${b},${a})`
+    let color = `rgba(${r},${g},${b},${a})`
     console.log(a)
-    box.style.backgroundColor = color ;
-    box.style.width= `${width}%`
-    box.style.height= `${height}%`
-    box.style.transform= `${transformX} ${transformY}`
+    box.style.backgroundColor = color;
+    box.style.width = `${width}%`
+    box.style.height = `${height}%`
+    box.style.transform = `${transformX} ${transformY}`
 
-     r = Math.floor(Math.random() * 255);
-     g = Math.floor(Math.random() * 255);
-     b = Math.floor(Math.random() * 255);
-     a = Number(Math.random().toPrecision(2)) ; 
+    r = Math.floor(Math.random() * 255);
+    g = Math.floor(Math.random() * 255);
+    b = Math.floor(Math.random() * 255);
+    a = Number(Math.random().toPrecision(2));
 
-     color= `rgba(${r},${g},${b},${a})`
+    color = `rgba(${r},${g},${b},${a})`
 
-     let box_outline = `${Math.floor(Math.random() * 25)}px`
+    let box_outline = `${Math.floor(Math.random() * 25)}px`
 
-     box.style.border=`${box_outline} solid ${color}`
+    box.style.border = `${box_outline} solid ${color}`
 
 }, 2000);
 
