e754fc574dde3749cbf2b33b28469a6bcf459f56
vortix2950 <vortix2950@ksu.edu>
2024-09-11 14:00:58 +0300

    box demo

--- script.js
+++ script.js
@@ -14,12 +14,16 @@ setInterval(() => {
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
+
+    let transformX = `translateX(${Math.floor(Math.random() * 255)}px)`;
+    let transformY = `translateY(${Math.floor(Math.random() * 255)}px)`;
     let color= `rgb(${r},${g},${b})`
 
 
-    box.style.color = color
+    box.style.backgroundColor = color ;
     box.style.width= `${width}px`
     box.style.height= `${height}px`
+    box.style.transform= `${transformX} ${transformY}`
 
     
 
