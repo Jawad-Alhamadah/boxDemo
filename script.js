c1f3f6c0533498c6661a5a486cbf27a772dd3390
vortix2950 <vortix2950@ksu.edu>
2024-09-11 14:29:20 +0300

    added opacity

--- script.js
+++ script.js
@@ -5,18 +5,18 @@ let box = document.getElementById("box")
 
 
 setInterval(() => {
-    console.log(Math.floor(Math.random() * 255))
+   
     let width =Math.floor(Math.random() * 100);
     let height=Math.floor(Math.random() * 100);
     
     let r = Math.floor(Math.random() * 255);
     let g = Math.floor(Math.random() * 255);
     let b = Math.floor(Math.random() * 255);
-
+    let a = Number(Math.random().toPrecision(2)) ;
     let transformX = `translateX(${Math.floor(Math.random() * 90)}vw)`;
     let transformY = `translateY(${Math.floor(Math.random() * 90)}vh)`;
-    let color= `rgb(${r},${g},${b})`
-
+    let color= `rgba(${r},${g},${b},${a})`
+    console.log(a)
     box.style.backgroundColor = color ;
     box.style.width= `${width}%`
     box.style.height= `${height}%`
@@ -25,7 +25,9 @@ setInterval(() => {
      r = Math.floor(Math.random() * 255);
      g = Math.floor(Math.random() * 255);
      b = Math.floor(Math.random() * 255);
-     color= `rgb(${r},${g},${b})`
+     a = Number(Math.random().toPrecision(2)) ; 
+
+     color= `rgba(${r},${g},${b},${a})`
 
      let box_outline = `${Math.floor(Math.random() * 25)}px`
 
